21873719f032aa9fb5c3273069dd87f22a2fb385
Leefinix <joaquinnleo@gmail.com>
2025-03-11 18:49:32 +0700

    i

---
+++ inter.py
@@ -0,0 +1,8 @@
+setx = {"green", "blue"}
+sety = {"blue", "yellow"}
+print("Original Elements: ")
+print(setx)
+print(sety)
+print("\n The intersection of the sets: ")
+setz = setx.intersection(sety)
+print(setz)
